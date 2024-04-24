4dcca99bbed8165865d1b5a4139cef0074f98904
Javier Lima <javi.limashelby@gmail.com>
2024-04-24 10:55:30 -0400

    feat: Inject Application layer

--- Gallery.API/Program.cs
+++ Gallery.API/Program.cs
@@ -1,11 +1,13 @@
 using FastEndpoints;
 using FastEndpoints.Swagger;
 using Gallery.API;
+using Gallery.Application;
 using Gallery.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPresentation();
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
