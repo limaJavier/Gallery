63e56409e1a18042cfb1d10ca4925871c05b134c
Javier Lima <javi.limashelby@gmail.com>
2024-04-24 10:14:05 -0400

    feat: Set infrastructure layer in Program.cs

--- Gallery.API/Program.cs
+++ Gallery.API/Program.cs
@@ -1,10 +1,12 @@
 using FastEndpoints;
 using FastEndpoints.Swagger;
 using Gallery.API;
+using Gallery.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPresentation();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
